867bbcb34ac900cbf11601908283e70bc6b52524
AbnerGZL <abner.gomez@tecsup.edu.pe>
2025-11-16 23:07:18 -0500

    reubicar estaticos

--- rumbia/settings.py
+++ rumbia/settings.py
@@ -16,7 +16,8 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-STATIC_URL = BASE_DIR / "staticfiles"
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 
 # Media files settings
